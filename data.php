27fe4be66f4f6a166b7afc4d6393a2b570d7b889
Sten-Erik Tool <Toolsten@users.noreply.github.com>
2016-10-11 13:23:28 +0300

    lisasin failid

---
+++ data.php
@@ -0,0 +1,38 @@
+<?php
+
+	require("functions.php");
+
+	//kui ei ole kasutaja id-d
+	if (!isset($_SESSION["userId"])) {
+		
+		//suunan sisselogimise lehele
+		header("Location: login.php");
+		exit();
+		
+		
+		
+	}
+	
+	//kui on ?logout aadressireal ss login välja
+	if (isset($_GET["logout"])) {
+		
+		session_destroy();
+		header("Location: login.php");
+	}
+	
+	$msg = "";
+	if(isset($_SESSION["message"])) {
+		
+		$msg = $_SESSION["message"];
+		
+		unset($_SESSION["message"]);
+	}
+	
+
+?>
+
+<h1>DATA</h1>
+<?=$msg;?>
+<p>Tere Tulemast <?=$_SESSION["userEmail"];?>!</p>
+<a href="?logout=1">Logi välja</a>
+
